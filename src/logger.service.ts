94e561f1c725d3c9eea2dddb39c096c9c778eed5
Jacob Dharandas <jacob.dharandas@prestashop.com>
2020-09-10 18:13:29 +0200

    Added nestjs-pino instead of pino logger

--- src/logger.service.ts
+++ src/logger.service.ts
@@ -1,77 +1,57 @@
-import { Injectable, LoggerService, Scope } from '@nestjs/common';
-import { isObject, isString } from '@nestjs/common/utils/shared.utils';
-import * as pino from 'pino';
-import { PinoLoggerConfig } from './logger.config';
+import { Injectable, LoggerService } from '@nestjs/common';
+import { PinoLogger } from 'nestjs-pino';
 
-@Injectable({ scope: Scope.TRANSIENT })
+@Injectable()
 export class Logger implements LoggerService {
-  private pino: pino.Logger;
-  private readonly options: any;
+  constructor(private readonly logger: PinoLogger) {}
 
-  constructor(options: PinoLoggerConfig = new PinoLoggerConfig()) {
-    this.options = options.get();
-    this.pino = pino(this.options);
+  setContext(context: string): void {
+    this.logger.setContext(context);
   }
 
-  setContext(context: string) {
-    this.pino = pino(this.options).child({ context });
+  verbose(message: any, context?: string, ...args: any[]) {
+    return context
+      ? this.logger.trace(
+          { context },
+          message,
+          ...args,
+        )
+      : this.logger.trace(message, ...args);
   }
 
-  setLabels(context: object) {
-    this.pino = this.pino.child({ ...context });
+  debug(message: any, context?: string, ...args: any[]) {
+    return context
+      ? this.logger.debug(
+          { context },
+          message,
+          ...args,
+        )
+      : this.logger.debug(message, ...args);
   }
 
-  info(msg: any, context?: string, ...args: any[]) {
+  log(message: any, context?: string, ...args: any[]) {
+    return context
+      ? this.logger.info({ context }, message, ...args)
+      : this.logger.info(message, ...args);
+  }
+
+  warn(message: any, context?: string, ...args: any[]) {
+    return context
+      ? this.logger.warn({ context }, message, ...args)
+      : this.logger.warn(message, ...args);
+  }
+
+  error(message: any, trace?: string, context?: string, ...args: any[]) {
     if (context) {
-      this.pino.child({ context }).info(msg, ...args);
-      return;
+      this.logger.error({ context, trace }, message, ...args);
+    } else if (trace) {
+      this.logger.error({ trace }, message, ...args);
+    } else {
+      this.logger.error(message, ...args);
     }
-    this.pino.info(msg, ...args);
   }
 
-  log(msg: any, context?: string, ...args: any[]) {
-    if (context) {
-      this.pino.child({ context }).info(msg, ...args);
-      return;
-    }
-    this.pino.info(msg, ...args);
-  }
-
-  error(msg: any, trace?: string, context?: string, ...args: any[]) {
-    this.pino.error(
-      {
-        message: isString(msg) ? msg : 'Error',
-        ...(isObject(msg) ? msg : {}),
-        trace,
-      },
-      ...args,
-    );
-  }
-
-  warn(msg: any, context?: string, ...args: any[]) {
-    if (context) {
-      this.pino.child({ context }).warn(msg, ...args);
-      return;
-    }
-    this.pino.warn(msg, ...args);
-  }
-
-  debug(msg: any, context?: string, ...args: any[]) {
-    if (context) {
-      this.pino.child({ context }).debug(msg, ...args);
-      return;
-    }
-    this.pino.debug(msg, context, ...args);
-  }
-
-  verbose(msg: any, context?: string, ...args: any[]) {
-    if (context) {
-      this.pino.child({ context }).verbose(msg, ...args);
-      return;
-    }
-    this.pino.verbose(msg, context, ...args);
-  }
-
+  // TODO Vincent : is this still necessary ?
   clone(): Logger {
     const copy = new (this.constructor as { new (): Logger })();
     Object.assign(copy, this);
