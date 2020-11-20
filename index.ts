17fa9db0fb1cd683b37dca483b5ec7f847a472ba
Jacob Dharandas <jacob.dharandas@prestashop.com>
2020-11-20 18:00:55 +0100

    3 bugs fixed

--- index.ts
+++ index.ts
@@ -1,5 +1,1 @@
-export * from './nestjs-ps-tools/src';
-export * from './nestjs-context/src';
-export * from './nestjs-correlation-id/src';
-export * from './nestjs-gcloud-trace/src';
-export * from './nestjs-pino-context/src';
+export * from './dist';
